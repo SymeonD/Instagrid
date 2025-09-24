f991a35710f7318cf25cc13dea975ad77ac93003
De Almeida Syméon <symeon.dealmeida@gmail.com>
2025-09-24 21:04:39 +0200

    Add image integration

--- Instagrid/src/app/app-grid/app-grid.ts
+++ Instagrid/src/app/app-grid/app-grid.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageService } from '../images.service';
-import { KtdGridModule } from '@katoid/angular-grid-layout';
+import { ktdGridCompact, KtdGridLayout, KtdGridLayoutItem, KtdGridModule } from '@katoid/angular-grid-layout';
+import { ktdTrackById } from '@katoid/angular-grid-layout';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-grid',
@@ -11,6 +15,29 @@ import { KtdGridModule } from '@katoid/angular-grid-layout';
 })
 
 export class AppGrid {
+
+  private placeholderLayout: KtdGridLayout = [
+    { id: '0', x: 0, y: 0, w: 1, h: 1 },
+    { id: '1', x: 1, y: 0, w: 1, h: 1 },
+    { id: '2', x: 2, y: 0, w: 1, h: 1 },
+    { id: '3', x: 0, y: 1, w: 1, h: 1 },
+    { id: '4', x: 1, y: 1, w: 1, h: 1 },
+  ];
+  
+  trackById = ktdTrackById;
+
+  cols = 3;
+  rowHeight = 100;
+  compactType: 'vertical' | 'horizontal' | null = 'vertical';
+  selectedItems: string[] = [];
+  // copiedItems: number
+  layout: KtdGridLayout = this.placeholderLayout;
+
+  resizeSubscription: Subscription | null = null;
+
+  private _isDraggingResizing: boolean = false;
+
+
   // Initiate items as an empty array of size 12
   items = Array.from({ length: 12 }, (_, i) => ({
     src: ``,
@@ -27,9 +54,14 @@ export class AppGrid {
       let newItems = [...this.items];
 
       images.forEach((img) => {
-        const targetIdx = Math.floor(img.num / 10) * 3 + img.num % 10;
+        // const targetIdx = Math.floor(img.num / 10) * 3 + img.num % 10;
         // Insert image at targetIdx, shifting others to the right
-        newItems.splice(targetIdx, 0, { ...img, groupId: groupId });
+        // newItems.splice(targetIdx, 0, { ...img, groupId: groupId });
+
+        // Add items to the grid
+        let newId : number = +this.addItemToLayout();
+
+        newItems.splice(newId, 0, { ...img, groupId: groupId });
       });
 
       // Truncate to grid size (12)
@@ -42,21 +74,30 @@ export class AppGrid {
     });
   }
 
-  moveGroup(groupId: number, targetIndexes: number[]) {
-    // Find all items in the group
-    const groupItems = this.items.filter(item => item.groupId === groupId);
+  /** Adds a grid item to the layout */
+    addItemToLayout(item?: KtdGridLayoutItem) : string{
+        let newLayoutItem: KtdGridLayoutItem | undefined = item;
+        if (!newLayoutItem) {
 
-    // Remove group items from their current positions
-    const otherItems = this.items.filter(item => item.groupId !== groupId);
 
-    // Insert group items at the target indexes
-    targetIndexes.forEach((targetIdx, i) => {
-      otherItems[targetIdx] = groupItems[i];
-    });
+            const maxId = this.layout.reduce(
+                (acc, cur) => Math.max(acc, parseInt(cur.id, 10)),
+                -1
+            );
+            const nextId = maxId + 1;
+            newLayoutItem = {
+                id: nextId.toString(),
+                x: -1,
+                y: -1,
+                w: 1,
+                h: 1
+            };
+        }
+        // Important: Don't mutate the array, create new instance. This way notifies the Grid component that the layout has changed.
+        this.layout = [newLayoutItem, ...this.layout];
+        this.layout = ktdGridCompact(this.layout, this.compactType, this.cols);
+        console.log('addItemToLayout', newLayoutItem);
 
-    // Fill any undefined slots with empty items
-    this.items = otherItems.map((item, i) =>
-      item ? item : { src: ``, alt: `Item ${i + 1}`, groupId: null }
-    );
-  }
+        return newLayoutItem.id;
+    }
 }
