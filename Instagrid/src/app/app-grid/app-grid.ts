ee135e7b345201c676abb7ffedc31bd5eb8dd960
De Almeida Syméon <symeon.dealmeida@gmail.com>
2025-09-24 21:58:12 +0200

    Add selection and movement

--- Instagrid/src/app/app-grid/app-grid.ts
+++ Instagrid/src/app/app-grid/app-grid.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageService } from '../images.service';
-import { ktdGridCompact, KtdGridLayout, KtdGridLayoutItem, KtdGridModule } from '@katoid/angular-grid-layout';
+import { KtdDragEnd, KtdDragStart, ktdGridCompact, KtdGridComponent, KtdGridLayout, KtdGridLayoutItem, KtdGridModule } from '@katoid/angular-grid-layout';
 import { ktdTrackById } from '@katoid/angular-grid-layout';
 import { Subscription } from 'rxjs';
+import { MatSelectChange } from '@angular/material/select';
 
 
 
@@ -23,17 +24,16 @@ export class AppGrid {
     { id: '3', x: 0, y: 1, w: 1, h: 1 },
     { id: '4', x: 1, y: 1, w: 1, h: 1 },
   ];
-  
+
+  @ViewChild(KtdGridComponent, {static: true}) grid: KtdGridComponent | undefined;
   trackById = ktdTrackById;
 
   cols = 3;
-  rowHeight = 100;
+  gridWidth = document.getElementById('image-grid-container')?.clientWidth || window.innerWidth*0.5;
+  rowHeight = 1350 / 1010 * (this.gridWidth / this.cols);
   compactType: 'vertical' | 'horizontal' | null = 'vertical';
   selectedItems: string[] = [];
-  // copiedItems: number
   layout: KtdGridLayout = this.placeholderLayout;
-
-  resizeSubscription: Subscription | null = null;
 
   private _isDraggingResizing: boolean = false;
 
@@ -44,6 +44,15 @@ export class AppGrid {
     alt: `Item ${i + 1}`,
     groupId: null // assign a number if part of a group
   }));
+
+  ngOnInit() {
+    // Update gridWidth and rowHeight on window resize
+    window.addEventListener('resize', () => {
+      this.gridWidth = document.getElementById('image-grid-container')?.clientWidth || window.innerWidth*0.5;
+      this.rowHeight = 1350 / 1010 * (this.gridWidth / this.cols);
+      console.log('Resized: ', this.gridWidth, this.rowHeight);
+    });
+  }
 
   constructor(private imageService: ImageService) {
     this.imageService.gridItems$.subscribe(images => {
@@ -74,6 +83,26 @@ export class AppGrid {
     });
   }
 
+  onDragStarted(event: KtdDragStart) {
+        this._isDraggingResizing = true;
+        console.log('onDragStarted', event);
+    }
+
+    onDragEnded(event: KtdDragEnd) {
+        this._isDraggingResizing = false;
+        console.log('onDragEnded', event);
+    }
+
+    onCompactTypeChange(change: MatSelectChange) {
+        console.log('onCompactTypeChange', change);
+        this.compactType = change.value;
+    }
+
+    onLayoutUpdated(layout: KtdGridLayout) {
+        console.log('onLayoutUpdated', layout);
+        this.layout = layout;
+    }
+
   /** Adds a grid item to the layout */
     addItemToLayout(item?: KtdGridLayoutItem) : string{
         let newLayoutItem: KtdGridLayoutItem | undefined = item;
@@ -100,4 +129,73 @@ export class AppGrid {
 
         return newLayoutItem.id;
     }
+
+    /**
+     * Check if 'selectedItem' is on the multi item selection
+     */
+    isItemSelected(selectedItem: KtdGridLayoutItem): boolean {
+        return this.selectedItems.includes(selectedItem.id);
+    }
+
+    /*
+     * Select an item outside of the group
+     */
+    pointerDownItemSelection(
+        event: MouseEvent,
+        selectedItem: KtdGridLayoutItem
+    ) {
+        const ctrlOrCmd = event.ctrlKey;
+        console.log('Clicked')
+        if (!ctrlOrCmd) {
+            const selectedItemExist = this.selectedItems.includes(
+                selectedItem.id
+            );
+            if (!selectedItemExist) {
+                // Click an element outside selection group
+                // Clean all selections and select the new item
+                if (event.button == 2) {
+                    this.selectedItems = [];
+                } else {
+                    this.selectedItems = [selectedItem.id];
+                }
+            }
+        }
+    }
+
+    /*
+     * Select an item inside the group or multiselect with Control button
+     */
+    pointerUpItemSelection(event: MouseEvent, selectedItem: KtdGridLayoutItem) {
+        const ctrlOrCmd = event.ctrlKey;
+        if (event.button !== 2) {
+            //Only select with primary button click
+            const selectedItemExist = this.selectedItems.includes(
+                selectedItem.id
+            );
+            if (ctrlOrCmd) {
+                if (selectedItemExist) {
+                    // Control + click an element inside the selection group
+                    if (!this._isDraggingResizing) {
+                        // If not dragging, remove the selected item from the group
+                        // TODO: Fix
+                        // this.selectedItems = ktdArrayRemoveItem(
+                        //     this.selectedItems,
+                        //   (itemId: string) => itemId === selectedItem.id
+                        // );
+                    }
+                } else {
+                    // Control + click an element outside the selection group
+                    // Add the new selected item to the current group
+                    this.selectedItems = [
+                        ...this.selectedItems,
+                        selectedItem.id
+                    ];
+                }
+            } else if (!this._isDraggingResizing && selectedItemExist) {
+                // Click an element inside the selection group
+                this.selectedItems = [selectedItem.id];
+            }
+        }
+    }
 }
+
