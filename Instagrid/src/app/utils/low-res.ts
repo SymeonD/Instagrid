2b431e781fafd4eea3412a55d568fe11dac3d08b
De Almeida Syméon <symeon.dealmeida@gmail.com>
2025-10-01 20:23:06 +0200

    Replace low res minim size

--- Instagrid/src/app/utils/low-res.ts
+++ Instagrid/src/app/utils/low-res.ts
@@ -3,6 +3,8 @@
 // Function to create a low resolution version of an image
 // Find the bigger side, and resize it to 200px, keep the original image aspect ratio
 export function createLowResImage(src: string): Promise<string> {
+    const maxSize = 400;
+
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.src = src;
@@ -12,11 +14,11 @@ export function createLowResImage(src: string): Promise<string> {
             if (!context) return;
             const aspectRatio = img.width / img.height;
             if (img.width > img.height) {
-                canvas.width = 200;
-                canvas.height = 200 / aspectRatio;
+                canvas.width = maxSize;
+                canvas.height = maxSize / aspectRatio;
             } else {
-                canvas.height = 200;
-                canvas.width = 200 * aspectRatio;
+                canvas.height = maxSize;
+                canvas.width = maxSize * aspectRatio;
             }
             context.drawImage(img, 0, 0, canvas.width, canvas.height);
             const newSrc = canvas.toDataURL('image/jpeg');
